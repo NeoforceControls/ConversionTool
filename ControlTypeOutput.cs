43d71bef3450692866c353f78629f201da928407
Christopher Crain <lakedoo23@gmail.com>
2016-06-24 00:38:08 -0400

    Added tab controls
    Added nested controls on the tabpages in a tab control.

--- ControlTypeOutput.cs
+++ ControlTypeOutput.cs
@@ -12,12 +12,12 @@ namespace UIParser
     {
         public string Declaration = string.Empty;
         public StringBuilder Instantiate = new StringBuilder();
-       
 
         public ControlTypeOutput(Control inputControl)
         {
 
             string type = inputControl.GetType().FullName.Replace("System.Windows.Forms.", "").Trim();
+
 
             Declaration = string.Format("        private {0} {1};", type,
                                                                inputControl.Name);
@@ -33,10 +33,46 @@ namespace UIParser
             Instantiate.AppendLine(string.Format("            this.{0}.Left = {1};", inputControl.Name, inputControl.Left));
             Instantiate.AppendLine(string.Format("            this.{0}.Text = \"{1}\";", inputControl.Name, inputControl.Text));
             Instantiate.AppendLine(string.Format("            this.{0}.TextColor = Color.{1};", inputControl.Name, GetColorName(inputControl.ForeColor)));
+
+            if (inputControl.GetType().Name == "TabControl")
+            {
+                HandleTabControls(inputControl);
+            }
+
             Instantiate.AppendLine(string.Format("            {0}.Add(this.{1});", ParseDesign.WindowName, inputControl.Name));
             Instantiate.AppendLine("");
             Instantiate.AppendLine(string.Format("            #endregion {0}", inputControl.Name));
             Instantiate.AppendLine("");
+        }
+
+        private void HandleTabControls(Control inputControl)
+        {
+            for (int i = 0; i < inputControl.Controls.Count; i++)
+            {
+                Instantiate.AppendLine(string.Format("            this.{0}.AddPage(\"{1}\");", inputControl.Name, inputControl.Controls[i].Name));
+                Instantiate.AppendLine(string.Format("            this.{0}.TabPages[{1}].Text = \"{2}\";", inputControl.Name, i, inputControl.Controls[i].Text));
+                Instantiate.AppendLine("            // Adding sub controls to tabs.");
+                Instantiate.AppendLine("");
+
+                foreach (Control control in inputControl.Controls[i].Controls)
+                {
+                    string subType = control.GetType().FullName.Replace("System.Windows.Forms.", "").Trim();
+                    Instantiate.AppendLine(string.Format("            {1} {0} = new {1}({2});", control.Name, subType, ParseDesign.ManagerName));
+                    Instantiate.AppendLine(string.Format("            {0}.Init();", control.Name));
+                    Instantiate.AppendLine(string.Format("            {0}.Name = \"{1}\";", control.Name, control.Name));
+                    Instantiate.AppendLine(string.Format("            {0}.Height = {1};", control.Name, control.Height));
+                    Instantiate.AppendLine(string.Format("            {0}.Width = {1};", control.Name, control.Width));
+                    Instantiate.AppendLine(string.Format("            {0}.Top = {1};", control.Name, control.Top));
+                    Instantiate.AppendLine(string.Format("            {0}.Left = {1};", control.Name, control.Left));
+                    Instantiate.AppendLine(string.Format("            {0}.Text = \"{1}\";", control.Name, control.Text));
+                    Instantiate.AppendLine(string.Format("            {0}.TextColor = Color.{1};", control.Name, GetColorName(control.ForeColor)));
+                    Instantiate.AppendLine("");
+                    Instantiate.AppendLine(string.Format("            this.{0}.TabPages[{1}].Add({2});", inputControl.Name, i, control.Name));
+                }
+
+
+            }
+
         }
 
         private string GetColorName(Color color)
